3eb5f61e8007280e516aaf40093051078b25be2e
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-18 08:44:01 +0200

    update idea

--- services/data-service.js
+++ services/data-service.js
@@ -9,12 +9,5 @@ return fetch('https://6436c5228205915d34fc6fd5.mockapi.io/series')
 return fetch('https://6436c5228205915d34fc6fd5.mockapi.io/series/' + serie.id, {method: 'PUT', headers: {'content-type':'application/json'}, body: JSON.stringify(serie)})
 .then(resp => resp.json())
     }
-
-<<<<<<< Updated upstream
-}
-=======
-
 }
 
-// https://6436c5228205915d34fc6fd5.mockapi.io/series
->>>>>>> Stashed changes

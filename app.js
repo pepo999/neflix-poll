e835e64fb5c7e87ae2ded8e76073b6052372ef5a
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-19 09:12:13 +0200

    update mobile

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ function fillCollectionFromServer(data) {
 function displayCollection() {
     const listOfSeries = document.getElementById('serie-list');
     listOfSeries.innerHTML = '';
-
     if (collection.series.length > 10) {
         for (let i = 0; i < 10; i++) {
             const serieAtIndexI = collection.series[i];
